5b72e4b9c2a2c3c1421a60ec93a7b2ecfd976389
Charlie Fisher <fosh@uw.edu>
2020-09-01 12:00:57 -0700

    adding any changes

--- app 2/run_speech_animation.py
+++ app 2/run_speech_animation.py
@@ -8,10 +8,12 @@ parser = ArgumentParser()
 parser.add_argument("--input", required=True, help="audio to render")
 parser.set_defaults(verbose=False)
 opt = parser.parse_args()
-print(opt)
+audio_file = opt.input
+dir_audio = "animation_audio/" + audio_file + ".mp3"
 
 va = sda.VideoAnimator(gpu=-1, model_path="grid")# Instantiate the animator
 
-vid, aud = va("example/image.bmp", "example/audio/speech1.mp3")
+vid, aud = va("animation_images/image.bmp", dir_audio)
 
-va.save_video(vid, aud, "../first-order-model-master/images/driver.mp4")
+output_file = "first-order-model-master/driver_videos/" + audio_file + ".mp4"
+va.save_video(vid, aud, output_file)
