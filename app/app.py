767d7e84519975060d169c756381640e581c8f05
Charlie Fisher <fosh@uw.edu>
2020-09-14 19:53:23 -0700

    test

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flaskwebgui import FlaskUI
-import WebPages as webp
-from pyshortcuts import make_shortcut
-
-make_shortcut('/Users/Charlie/Desktop/PillPackDispenser/app/app.py', name='Pill Pack Dispenser', icon='/Users/Charlie/Desktop/PillPackDispenser/app/app_icon')
-if __name__ == '__main__':
-    webp.ui.run()
-
- 
